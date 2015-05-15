58e40131ea1b91079646308553998160ef2f388d
Mike Sergeant <sergeant.mj@gmail.com>
2015-05-14 21:04:13 -0400

    Now using count instead of sum variable

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,10 +1,7 @@
 class Hamming
   def self.compute(a ,b)
-    sum = 0
-    0.upto(a.length - 1) do |index|
-      sum += 1 if (a[index] != b[index])
+    0.upto(a.length - 1).count do |index|
+      a[index] != b[index]
     end
-
-    sum
   end
 end
