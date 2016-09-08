07be5629420d5b10452df2a4becff330652b80a2
Mike Sergeant <sergeant.mj@gmail.com>
2016-09-07 21:00:21 -0400

    First iteration of space-age

---
+++ elixir/space-age/space_age.exs
@@ -0,0 +1,28 @@
+defmodule SpaceAge do
+  @type planet :: :mercury | :venus | :earth | :mars | :jupiter
+                | :saturn | :neptune | :uranus
+
+  @doc """
+  Return the number of years a person that has lived for 'seconds' seconds is
+  aged on 'planet'.
+  """
+  @spec age_on(planet, pos_integer) :: float
+  def age_on(planet, seconds) do
+    seconds
+    |> Kernel./(31_557_600)
+    |> Kernel./(period(planet))
+  end
+
+  defp period(planet) do
+    case planet do
+      :earth   -> 1
+      :mercury -> 0.2408467
+      :venus   -> 0.61519726
+      :mars    -> 1.8808158
+      :jupiter -> 11.862615
+      :saturn  -> 29.447498
+      :uranus  -> 84.016846
+      :neptune -> 164.79132
+    end
+  end
+end
