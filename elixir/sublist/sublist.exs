1052d06cc8913f908ffa2fce4b64479702923f29
Mike Sergeant <sergeant.mj@gmail.com>
2016-10-13 01:20:51 -0400

    First iteration of sublist

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -16,6 +16,48 @@ defmodule Sublist do
   end
 
   def compare(a, b) do
-    :unequal
+    cond do
+      recursive_compare(a, b) |> Enum.count > 0 -> :sublist
+      recursive_compare(b, a) |> Enum.count > 0 -> :superlist
+      true-> :unequal
+    end
+  end
+
+  defp recursive_compare([], candidate_element, [candidate_element | _]) do
+    IO.puts "Empty"
+    true
+  end
+
+  defp recursive_compare([x], x, [x]) do
+    IO.puts "All Match"
+    true
+  end
+
+  defp recursive_compare([x], x, [x | _]) do
+    true
+  end
+
+  defp recursive_compare([matching_first_element | remaining_candidates], matching_first_element, [matching_first_element | remaining_elements]) do
+    #IO.puts "More recursion is needed"
+    #IO.inspect [matching_first_element | remaining_candidates]
+    #IO.puts matching_first_element
+    #IO.inspect [matching_first_element | remaining_elements]
+    [new_candidate | _ ] = remaining_candidates
+    recursive_compare(remaining_candidates, new_candidate, remaining_elements)
+  end
+
+  defp recursive_compare(_, _, _) do
+    false
+  end
+
+  defp recursive_compare(a, b) do
+    Enum.reduce_while(b, b,  fn (_, acc) ->
+      [first_of_a | rest_of_a] = a
+      [first_of_b | rest_of_b] = acc
+      cond do
+        recursive_compare(a, first_of_a, acc) -> {:halt, acc}
+        true -> {:cont, rest_of_b}
+      end
+    end)
   end
 end
