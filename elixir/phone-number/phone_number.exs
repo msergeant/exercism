df21450c5b4e848203379d1d58d3c81fa5e07b8d
Mike Sergeant <sergeant.mj@gmail.com>
2016-11-03 07:03:15 -0400

    First iteration of phone-number

---
+++ elixir/phone-number/phone_number.exs
@@ -0,0 +1,75 @@
+defmodule Phone do
+  @doc """
+  Remove formatting from a phone number.
+
+  Returns "0000000000" if phone number is not valid
+  (10 digits or "1" followed by 10 digits)
+
+  ## Examples
+
+  iex> Phone.number("123-456-7890")
+  "1234567890"
+
+  iex> Phone.number("+1 (303) 555-1212")
+  "3035551212"
+
+  iex> Phone.number("867.5309")
+  "0000000000"
+  """
+  @spec number(String.t) :: String.t
+  def number(raw) do
+    new_num = Regex.replace(~r/\(|\)| |-|\./, raw, "")
+    l = String.length(new_num)
+    cond do
+      l == 11 && String.first(new_num) == "1" -> String.slice(new_num, 1..-1)
+      l == 10 -> new_num
+      true -> "0000000000"
+    end
+  end
+
+  @doc """
+  Extract the area code from a phone number
+
+  Returns the first three digits from a phone number,
+  ignoring long distance indicator
+
+  ## Examples
+
+  iex> Phone.area_code("123-456-7890")
+  "123"
+
+  iex> Phone.area_code("+1 (303) 555-1212")
+  "303"
+
+  iex> Phone.area_code("867.5309")
+  "000"
+  """
+  @spec area_code(String.t) :: String.t
+  def area_code(raw) do
+    number(raw)
+    |> String.slice(0..2)
+  end
+
+  @doc """
+  Pretty print a phone number
+
+  Wraps the area code in parentheses and separates
+  exchange and subscriber number with a dash.
+
+  ## Examples
+
+  iex> Phone.pretty("123-456-7890")
+  "(123) 456-7890"
+
+  iex> Phone.pretty("+1 (303) 555-1212")
+  "(303) 555-1212"
+
+  iex> Phone.pretty("867.5309")
+  "(000) 000-0000"
+  """
+  @spec pretty(String.t) :: String.t
+  def pretty(raw) do
+    num = number(raw)
+    "(" <> area_code(num) <> ") " <> String.slice(num, 3..5) <> "-" <> String.slice(num, 6..-1)
+  end
+end
