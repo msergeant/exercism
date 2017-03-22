8f22506be1dd7efff37ded078cc8b4dfdb73f170
Mike Sergeant <sergeant.mj@gmail.com>
2017-03-21 21:41:16 -0400

    First iteration of difference of squares

---
+++ elixir/difference-of-squares/difference_of_squares.exs
@@ -0,0 +1,33 @@
+defmodule Squares do
+  @moduledoc """
+  Calculate sum of squares, square of sums, difference between two sums from 1 to a given end number.
+  """
+
+  @doc """
+  Calculate sum of squares from 1 to a given end number.
+  """
+  @spec sum_of_squares(pos_integer) :: pos_integer
+  def sum_of_squares(number) do
+    1..number
+    |> Enum.reduce(0, &(&1 * &1 + &2))
+  end
+
+  @doc """
+  Calculate square of sums from 1 to a given end number.
+  """
+  @spec square_of_sums(pos_integer) :: pos_integer
+  def square_of_sums(number) do
+    1..number
+    |> Enum.reduce(0, &(&1 + &2))
+    |> (fn(x) -> x * x end).()
+  end
+
+  @doc """
+  Calculate difference between sum of squares and square of sums from 1 to a given end number.
+  """
+  @spec difference(pos_integer) :: pos_integer
+  def difference(number) do
+    square_of_sums(number) - sum_of_squares(number)
+  end
+
+end
