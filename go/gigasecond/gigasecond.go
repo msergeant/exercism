d959e7a6b45368bad8eefd0d40fbf7eabfd3955d
Mike Sergeant <sergeant.mj@gmail.com>
2017-09-24 22:15:48 -0400

    Give 'magic' number a name

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -4,8 +4,9 @@ package gigasecond
 import "time"
 
 const testVersion = 4
+const billion = 1e9
 
 // AddGigasecond returns the time 1 billion seconds later
 func AddGigasecond(t time.Time) time.Time {
-	return t.Add(1000 * 1000 * 1000 * time.Second)
+	return t.Add(billion * time.Second)
 }
