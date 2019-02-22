5c9371a7b09f0229125c2d55343492f2f736f9a4
Mike Sergeant <sergeant.mj@gmail.com>
2019-02-21 22:33:30 -0500

    Second iteration. This time without regex

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,18 +1,8 @@
-import re
-
 def abbreviate(words):
+    without_special_chars = words.replace("-", " ")
+    without_special_chars = without_special_chars.replace("_", " ")
     return "".join(
         list(
-            map(_first_letter_upper, _split_spaces_and_dashes(words))
+            map(lambda x: x[0].upper(), without_special_chars.split())
         )
     )
-
-def _split_spaces_and_dashes(string):
-    return re.sub("-", " ", string).split()
-
-def _first_letter_upper(string):
-    without_special_chars = re.sub("\W+|_+", "", string)
-    if len(without_special_chars) > 0:
-        return without_special_chars[0].upper()
-    else:
-        return ""
