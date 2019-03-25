e3be28ca9e54712c42c1e07ab5f4f3026f572e60
Mike Sergeant <sergeant.mj@gmail.com>
2019-03-24 22:08:20 -0400

    Second hamming iteration; using list comprehension

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,13 +1,7 @@
 def distance(strand_a, strand_b):
-    if strand_a == strand_b:
-        return 0
-    elif len(strand_a) != len(strand_b):
+    if len(strand_a) != len(strand_b):
         raise ValueError("Strings must be same length")
-    else:
-        total = 0
-        for i in range(len(strand_a)):
-            if strand_a[i] != strand_b[i]:
-                total += 1
 
-        return total
+    return sum([ a != b for a, b in zip(strand_a, strand_b) ])
+
 
