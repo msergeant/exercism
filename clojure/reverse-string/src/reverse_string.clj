7bd61c450e6bd6276497e7c28ca67e6d87811ea7
Mike Sergeant <sergeant.mj@gmail.com>
2019-04-01 08:17:54 -0400

    Remove some unnecessary stuff

--- clojure/reverse-string/src/reverse_string.clj
+++ clojure/reverse-string/src/reverse_string.clj
@@ -2,11 +2,10 @@
 
 (defn reverse-string [s] ;; <- arglist goes here
   ;(apply str (seq (recursive-reverse [] s))))
-  (let [r ""
-        i (count s)]
+  (let [r ""]
     (loop [r ""
            f s]
       (if (= 0 (count f))
         r
-        (recur (str r (str (last f)))
+        (recur (str r (last f))
                (subs s 0 (- (count f) 1)))))))
