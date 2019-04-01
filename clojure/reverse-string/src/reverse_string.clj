f9e9bd3fcc824a1d7cddc2065e4a15ed2905e563
Mike Sergeant <sergeant.mj@gmail.com>
2019-03-31 21:20:46 -0400

    Second iteration of reverse-string. Moved recursion into loop

--- clojure/reverse-string/src/reverse_string.clj
+++ clojure/reverse-string/src/reverse_string.clj
@@ -1,14 +1,12 @@
 (ns reverse-string)
 
-(defn recursive-reverse [acc s]
-  (let
-    [last-char (last s)
-     len (.length s)]
-    (cond
-      (= 0 len) acc
-      :else (recursive-reverse
-              (conj acc last-char)
-              (subs s 0 (- len 1))))))
-
 (defn reverse-string [s] ;; <- arglist goes here
-  (apply str (seq (recursive-reverse [] s))))
+  ;(apply str (seq (recursive-reverse [] s))))
+  (let [r ""
+        i (count s)]
+    (loop [r ""
+           f s]
+      (if (= 0 (count f))
+        r
+        (recur (str r (str (last f)))
+               (subs s 0 (- (count f) 1)))))))
