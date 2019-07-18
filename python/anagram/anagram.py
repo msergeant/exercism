a9b8cf3b30b15841c11c6000e66ef0551394daed
Mike Sergeant <sergeant.mj@gmail.com>
2019-07-18 08:26:05 -0400

    Second iteration of python anagram. Uses sorted instead of letter counts.

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -2,13 +2,7 @@ def find_anagrams(word, candidates):
     return [w for w in candidates if __is_anagram(word, w)]
 
 def __is_anagram(w1, w2):
-    if len(w1) != len(w2):
-        return False
-
     word1 = w1.lower()
     word2 = w2.lower()
-    count1 = {l: word1.count(l) for l in word1}
-    count2 = {l: word2.count(l) for l in word2}
 
-    return  word1 != word2 and count1 == count2
-
+    return  word1 != word2 and sorted(word1) == sorted(word2)
